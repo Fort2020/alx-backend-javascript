12122251d7ea7789ddced6da196c4e509aca6292
fort2020 <frankfortdata@gmail.com>
2023-09-20 22:16:51 +0000

    E Be Like Say Task 2 Chop Witch

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,18 +1,18 @@
 function calculateNumber(type, a, b) {
-    const rounded_a = Math.round(a);
-    const rounded_b = Math.round(b);
+    const aRounded = Math.round(a);
+    const aRounded = Math.round(b);
     
     if (type === 'SUM') {
-      return rounded_a + rounded_b;
+      return aRounded_a + aRounded;
     }
     if (type === 'SUBTRACT') {
-      return rounded_a - rounded_b;
+      return aRounded - aRounded;
     }
     if (type === 'DIVIDE') {
-      if (rounded_b === 0) {
+      if (bRounded === 0) {
         return 'Error';
       }
-      return rounded_a / rounded_b;
+      return aRounded / bRounded;
     }
   }
   
