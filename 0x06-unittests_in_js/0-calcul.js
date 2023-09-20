10fbc14d857731fbd12fb36ab5c4a93ea42cb92b
fort2020 <frankfortdata@gmail.com>
2023-09-20 21:43:22 +0000

    Testing Everything That Required to be Tested

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,10 +1,3 @@
-function calculateNumber(a, b){
-    return Math.round(a) + Math.round(b);
-}
-
-module.exports = calculateNumber;
-
-------------------------------OR ---------------
 const calculateNumber = (a, b) => {
   const aRound = Math.round(a);
   const bRound = Math.round(b);
